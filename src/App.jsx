216c7da9e0e5e5bfe371081ec6ab28714858cdae
uzair171 <jabransaleem171@gmail.com>
2025-06-27 11:55:04 +0500

    created user google Auth with firsebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Shop from "./pages/Shop/Shop";
+import SignIn from "./pages/SignIn/SignIn";
 
 const App = () => {
   return (
     <Routes>
       <Route path={"/"} element={<NavigationBar />}>
         <Route index element={<Home />} />
-        <Route path={"/shop"} element={<Shop />} />
+        <Route path={"shop"} element={<Shop />} />
+        <Route path={"sign-in"} element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API,
+  authDomain: "monowear-55ba5.firebaseapp.com",
+  projectId: "monowear-55ba5",
+  storageBucket: "monowear-55ba5.firebasestorage.app",
+  messagingSenderId: "692248184729",
+  appId: "1:692248184729:web:afd93140b34ec1722bf508",
+};
+
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return userDocRef;
+};
