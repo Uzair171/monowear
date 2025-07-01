b0b40ecb0042b7ea8a915adf0f86a3788b837986
uzair171 <jabransaleem171@gmail.com>
2025-07-01 11:12:53 +0500

    shop page filtered (4 products only)

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,24 +1,19 @@
 import { Fragment, useContext } from "react";
 import { ProductsContext } from "../../context/productContext";
-import ProductCard from "../../components/ProductCard/ProductCard";
+
 import "./shop.style.scss";
+import CategoryPreview from "../../components/CategoryPreview/CategoryPreview";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <Fragment>
-      {Object.keys(products).map((title) => (
-        <Fragment key={title}>
-          <h2>{title.toUpperCase()}</h2>
-          <div className="products-container">
-            {products[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-container">
+      {Object.keys(products).map((title) => {
+        const product = products[title];
+        return <CategoryPreview key={title} title={title} products={product} />;
+      })}
+    </div>
   );
 };
 
